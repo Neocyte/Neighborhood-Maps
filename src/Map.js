0fe760686914e41b6fe2332b5580163651f7ff98
Mason Wu <masonwu8462@yahoo.com>
2018-07-31 18:53:34 -0400

    Doc: Simplify comments and organize functions with comments

--- src/Map.js
+++ src/Map.js
@@ -11,11 +11,13 @@ class Map extends React.Component {
   var markers = [];
 
   initMap() {
+    // Creates the default map
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.7160857, lng: -73.9990696},
       zoom: 17
     });
 
+    // Array of locations
     var locations = [
       {title: 'Hong Kong Supermarket', location: {lat: 40.7158702, lng: -74.0016874}},
       {title: 'New York Mart', location: {lat: 40.7162727, lng: -73.9988872}},
@@ -26,19 +28,18 @@ class Map extends React.Component {
 
     var largeInfowindow = new google.maps.InfoWindow();
 
-    // Style the markers a bit. This will be our listing marker icon.
+    // Default icons
     var defaultIcon = makeMarkerIcon('0091ff');
 
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
+    // Highlighted Icons
     var highlightedIcon = makeMarkerIcon('FFFF24');
 
-    // The following group uses the location array to create an array of markers on initialize.
+    //-----------------------------MARKER---------------------------------------
+
+    // Loops through each location to create a marker for each location
     for (var i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
       var position = locations[i].location;
       var title = locations[i].title;
-      // Create a marker per location, and put into markers array.
       var marker = new google.maps.Marker({
         map: map,
         position: position,
@@ -48,16 +49,15 @@ class Map extends React.Component {
         id: i
       });
 
-      // Push the marker to our array of markers.
+      // Push the marker to our array of markers
       markers.push(marker);
 
-      // Create an onclick event to open an infowindow at each marker.
+      // Open an infowindow at each marker on click
       marker.addListener('click', function() {
         populateInfoWindow(this, largeInfowindow);
       });
 
-      // Two event listeners - one for mouseover, one for mouseout,
-      // to change the colors back and forth.
+      // Switches between highlighted and default icons on hover
       marker.addListener('mouseover', function() {
         this.setIcon(highlightedIcon);
       });
@@ -67,24 +67,34 @@ class Map extends React.Component {
     }
   }
 
-  // This function populates the infowindow when the marker is clicked. We'll only allow
-  // one infowindow which will open at the marker that is clicked, and populate based
-  // on that markers position.
+  // Takes in a color and creates a custom marker icon
+  makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21,34));
+    return markerImage;
+  }
+
+  //-----------------------------INFOWINDOW-------------------------------------
+
+  // Adds information to each marker's infowindow
   populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
+    // Check to make sure the infowindow is not already opened on this marker
     if (infowindow.marker != marker) {
-      // Clear the infowindow content to give the streetview time to load.
+      // Clear the infowindow content to give the streetview time to load
       infowindow.setContent('');
       infowindow.marker = marker;
-      // Make sure the marker property is cleared if the infowindow is closed.
+      // Make sure the marker property is cleared if the infowindow is closed
       infowindow.addListener('closeclick', function() {
         infowindow.marker = null;
       });
       var streetViewService = new google.maps.StreetViewService();
       var radius = 50;
-      // In case the status is OK, which means the pano was found, compute the
-      // position of the streetview image, then calculate the heading, then get a
-      // panorama from that and set the options
+      // Fetches streetview information
       function getStreetView(data, status) {
         if (status == google.maps.StreetViewStatus.OK) {
           var nearStreetViewLocation = data.location.latLng;
@@ -105,27 +115,14 @@ class Map extends React.Component {
             '<div>No Street View Found</div>');
         }
       }
-      // Use streetview service to get the closest streetview image within
-      // 50 meters of the markers position
+      // Fetches closest streetview image within 50 meters of the markers position
       streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-      // Open the infowindow on the correct marker.
+      // Open the infowindow on the correct marker
       infowindow.open(map, marker);
     }
   }
 
-  // This function takes in a COLOR, and then creates a new marker
-  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-  // of 0, 0 and be anchored at 10, 34).
-  makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21,34));
-    return markerImage;
-  }
+  //-----------------------------RENDER-----------------------------------------
 
   render() {
     return (
