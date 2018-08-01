1d3975a65db9870eab10aa17933dbe1324596e94
Mason Wu <masonwu8462@yahoo.com>
2018-08-01 17:05:51 -0400

    Feat: Add render content to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Map from './Map';
-import List from './List';
-import Filter from './Filter';
 import './App.css';
 
 
@@ -25,13 +23,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Route exact path="/" render={() => (
-          <div class="container">
-            <h1>Great Eats at Chinatown, New York</h1>
-            <Map />
-          </div>
-        )}/>
+      <div>
+      
+        <a className="menu" tabIndex="0">
+          <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+          </svg>
+        </a>
+
+        <h1 className="heading">Great Eats at Chinatown, New York</h1>
+
+        <Map />
+
       </div>
     )
   }
