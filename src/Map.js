4cce9c857db6d7b752f7470975ab5cee373c0f5d
Mason Wu <masonwu8462@yahoo.com>
2018-08-01 16:24:29 -0400

    Refactor: Change code to ES6 and implement state

--- src/Map.js
+++ src/Map.js
@@ -1,75 +1,101 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Markers from './Markers';
-import InfoWindow from './InfoWindow';
+import ReactDOM from 'react-dom';
 
 class Map extends React.Component {
-  static propTypes = {
 
-  };
-
-  var markers = [];
-
-  initMap() {
-    // Creates the default map
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7160857, lng: -73.9990696},
-      zoom: 17
-    });
-
-    // Array of locations
-    var locations = [
+  state = {
+    locations = [
       {title: 'Hong Kong Supermarket', location: {lat: 40.7158702, lng: -74.0016874}},
       {title: 'New York Mart', location: {lat: 40.7162727, lng: -73.9988872}},
       {title: 'Q Q Bakery', location: {lat: 40.7139409, lng: -73.9972886}},
       {title: 'Vivi Bubble Tea', location: {lat: 40.7153132, lng: -73.9996167}},
       {title: 'Tasty Hand-Pulled Noodles', location: {lat: 40.714193, lng: -73.9982381}}
-    ];
+    ],
+    query: '',
+    markers: [],
+    infowindow: new this.props.google.maps.InfoWindow(),
+    defaultIcon: makeMarkerIcon('0091ff'),
+    highlightedIcon: makeMarkerIcon('FFFF24')
+  };
 
-    var largeInfowindow = new google.maps.InfoWindow();
+  componentDidMount() {
+    this.initMap();
+    this.clickLocationList();
+  }
 
-    // Default icons
-    var defaultIcon = makeMarkerIcon('0091ff');
+  //------------------------------MAP-------------------------------------------
 
-    // Highlighted Icons
-    var highlightedIcon = makeMarkerIcon('FFFF24');
+  initMap() {
+    // Creates the default map
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7160857, lng: -73.9990696},
+      zoom: 17
+    });
+    this.addMarkers();
+  }
 
-    //-----------------------------MARKER---------------------------------------
+  //-------------------------Location List--------------------------------------
+
+  clickLocationList = () => {
+    const {infowindow} = this.state;
+
+    const showLocation = (event) => {
+      const {markers} = this.state
+      const markerIndex =
+        markers.findIndex(m => m.title.toLowerCase() === e.target.innerText.toLowerCase());
+        this.populateInfoWindow(markers[markerIndex], infowindow);
+    };
+
+    document.querySelector('.locations-list').addEventListener('click', function (event) {
+      if (e.target && e.target.nodeName === "LI") {
+        showLocation(event);
+      }
+    });
+  }
+
+  //-----------------------------Filter-----------------------------------------
+
+  handleQuery = (event) => {
+    this.setState({query: event.target.value});
+  }
+
+  //-----------------------------MARKER-----------------------------------------
+
+  addMarkers = () => {
+    const bounds = new google.maps.LatLngBounds();
 
     // Loops through each location to create a marker for each location
-    for (var i = 0; i < locations.length; i++) {
-      var position = locations[i].location;
-      var title = locations[i].title;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        icon: defaultIcon,
-        animation: google.maps.Animation.DROP,
-        id: i
+    this.state.locations.forEach((location, index) => {
+      const marker = new google.maps.Marker({
+        position: location.location,
+        map: this.map,
+        title: location.title
+        icon: this.state.defaultIcon,
+        animation: google.maps.Animation.DROP
       });
 
       // Push the marker to our array of markers
-      markers.push(marker);
+      this.setState((state) => ({
+        markers: [...state.markers, marker]
+      }));
 
       // Open an infowindow at each marker on click
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
+      marker.addListener('click', () => {
+        this.populateInfoWindow(marker, this.state.infowindow);
       });
 
       // Switches between highlighted and default icons on hover
-      marker.addListener('mouseover', function() {
-        this.setIcon(highlightedIcon);
+      marker.addListener('mouseover', () => {
+        this.setIcon(this.state.highlightedIcon);
       });
-      marker.addListener('mouseout', function() {
-        this.setIcon(defaultIcon);
+      marker.addListener('mouseout', () => {
+        this.setIcon(this.state.defaultIcon);
       });
     }
-  }
 
   // Takes in a color and creates a custom marker icon
-  makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
+  makeMarkerIcon = (markerColor) => {
+    const markerImage = new google.maps.MarkerImage(
       'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
       '|40|_|%E2%80%A2',
       new google.maps.Size(21, 34),
@@ -82,43 +108,17 @@ class Map extends React.Component {
   //-----------------------------INFOWINDOW-------------------------------------
 
   // Adds information to each marker's infowindow
-  populateInfoWindow(marker, infowindow) {
+  populateInfoWindow = (marker, infowindow) => {
     // Check to make sure the infowindow is not already opened on this marker
-    if (infowindow.marker != marker) {
-      // Clear the infowindow content to give the streetview time to load
-      infowindow.setContent('');
+    if (infowindow.marker !== marker) {
       infowindow.marker = marker;
+      infowindow.setContent('<h3>${marker.title}</h3>');
       // Make sure the marker property is cleared if the infowindow is closed
-      infowindow.addListener('closeclick', function() {
+      infowindow.addListener('closeclick', () => {
         infowindow.marker = null;
       });
-      var streetViewService = new google.maps.StreetViewService();
-      var radius = 50;
-      // Fetches streetview information
-      function getStreetView(data, status) {
-        if (status == google.maps.StreetViewStatus.OK) {
-          var nearStreetViewLocation = data.location.latLng;
-          var heading = google.maps.geometry.spherical.computeHeading(
-            nearStreetViewLocation, marker.position);
-            infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-            var panoramaOptions = {
-              position: nearStreetViewLocation,
-              pov: {
-                heading: heading,
-                pitch: 30
-              }
-            };
-          var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), panoramaOptions);
-        } else {
-          infowindow.setContent('<div>' + marker.title + '</div>' +
-            '<div>No Street View Found</div>');
-        }
-      }
-      // Fetches closest streetview image within 50 meters of the markers position
-      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
       // Open the infowindow on the correct marker
-      infowindow.open(map, marker);
+      infowindow.open(this.map, marker);
     }
   }
 
