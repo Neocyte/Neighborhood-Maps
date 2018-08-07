8f10c9f8e2cac0f35d6f22496dee32560919dc42
Mason Wu <masonwu8462@yahoo.com>
2018-08-07 16:33:41 -0400

    Feat: Add link to Foursquare Places API

--- src/Map.js
+++ src/Map.js
@@ -238,6 +238,8 @@ class Map extends React.Component {
               (<li key={index} role="button" tabIndex="0">{m.title}</li>))
           }
           </ul>
+
+          <a href="https://developer.foursquare.com/docs/api/venues/details" className="apiLink" tabIndex="0">Powered By Foursquare Places API</a>
         </div>
 
         <div role="application" className="map" id="map"></div>
